1f9012421edb25d1427a93ee9d4e6093d78fd5b9
psimas21 <yescs4real@gmail.com>
2022-06-07 14:20:35 +0100

    correct post page title

--- resources/js/Pages/Post.vue
+++ resources/js/Pages/Post.vue
@@ -3,7 +3,7 @@
         <main class="flex-1">
             <div class="py-6">
             <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-                <h1 class="text-2xl font-semibold text-gray-900">Post Report</h1>
+                <h1 class="text-2xl font-semibold text-gray-900">Post Result</h1>
             </div>
             <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
                <Modal
